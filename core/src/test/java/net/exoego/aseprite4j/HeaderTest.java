32742837d77b72587b8394cf1e11279b03b4c422
exoego <ytatsuno.jp@gmail.com>
2025-05-06 12:37:38 +0900

    viewer

--- core/src/test/java/net/exoego/aseprite4j/HeaderTest.java
+++ core/src/test/java/net/exoego/aseprite4j/HeaderTest.java
@@ -2,6 +2,7 @@ package net.exoego.aseprite4j;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import org.junit.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -16,9 +17,27 @@ public class HeaderTest {
             "tags3x123reps, 1282",
             "file-tests-props, 2300",
     })
-    void addition(String filename, int expectedSize) throws URISyntaxException {
+    void getFileSize(String filename, int expectedSize) throws URISyntaxException {
         var path = Paths.get(HeaderTest.class.getResource("/aseprite/sprites/" + filename + ".aseprite").toURI());
         var header = Header.read(path);
         assertThat(header.getFileSize()).isEqualTo(expectedSize);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "cut_paste, 1",
+            "1empty3, 3",
+            "tags3x123reps, 9",
+            "file-tests-props, 6",
+            "point4frames, 4",
+            "2f-index-3x3, 2",
+            "4f-index-4x4, 4",
+            "slices-moving, 4",
+            "2x2tilemap2x2tile, 1"
+    })
+    void getFrames(String filename, int expectedSize) throws URISyntaxException {
+        var path = Paths.get(HeaderTest.class.getResource("/aseprite/sprites/" + filename + ".aseprite").toURI());
+        var header = Header.read(path);
+        assertThat(header.getFrames()).isEqualTo(expectedSize);
     }
 }
